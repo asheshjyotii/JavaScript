25d2b8bd4664a9dbc54368e27b9e73215be0753c
asheshjyotii <asheshjyotimajumdar3@gmail.com>
2024-06-25 23:57:41 +0530

    objects in javascript

--- basic_02_array/objects.js
+++ basic_02_array/objects.js
@@ -1,6 +1,8 @@
 // two ways to make object: constructors and object literals
 
 let jsUser = {
-    name : "Ashesh"
+    name : "Ashesh",
+    age : 20
 } // object literal
 console.log(jsUser.name)
+console.log(jsUser.age)
